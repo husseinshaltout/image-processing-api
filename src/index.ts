a70ba9b71d0f38c1a2a1c77967b41b61b01c4f51
Hussein Shaltout <hussein.shaltout@gmail.com>
2022-07-22 07:01:03 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+// create an instance server
+const app: Application = express();
+// HTTP request logger middleware
+app.use(morgan('short'));
+
+// add routing for / path
+app.get('/', (req: Request, res: Response) => {
+    res.json({
+        message: 'Hello World 🌍'
+    });
+});
+
+// start express server
+app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is starting at http://localhost:${PORT}`);
+});
+
+export default app;
